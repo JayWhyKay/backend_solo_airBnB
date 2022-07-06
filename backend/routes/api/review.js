11781115a99de31d9d1e436bf75e7bbe0cf13ef4
James K <jameskim369@gmail.com>
2022-07-06 11:09:19 -0700

    Finished spots

--- backend/routes/api/review.js
+++ backend/routes/api/review.js
@@ -1,12 +1,29 @@
 const express = require('express')
 
 const { requireAuth } = require('../../utils/auth');
-const { User, Spot } = require('../../db/models');
+const { User, Spot, Review } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const validateListing = async (req, res, next) => {
+    const exists = await Spot.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+    if(exists) return next()
+
+    const err = new Error("Spot could not be found");
+    err.status = 404;
+    return next(err);
+}
+
+router.get("/reviews", requireAuth, async(req,res) => {
+    const Reviews = await Review.findAll({where:{userId: req.user.id}})
+    console.log(Reviews)
+})
 
 
 
